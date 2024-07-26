47bae901dfb3d117fbcf898bdc2c1533d0f7354e
fangronghua@corp.netease.com <392572115@qq.com>
2024-07-26 11:32:18 +0800

    215

--- 215.findKthLargest/main.go
+++ 215.findKthLargest/main.go
@@ -10,11 +10,52 @@ package main
 示例 2:
 输入: [3,2,3,1,2,4,5,5,6], k = 4
 输出: 4
- */
-func main()  {
-
+*/
+func main() {
+	//nums := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
+	nums := []int{4, 3, 2, 5}
+	k := findKthLargest(nums, 1)
+	println(k)
 }
+
+
 
 func findKthLargest(nums []int, k int) int {
-
+	TopKSplit(nums, len(nums)-k, 0, len(nums)-1)
+	return nums[len(nums)-k]
 }
+
+func Parition(nums []int, start, stop int)int{
+	if start >= stop{
+		return -1
+	}
+	pivot := nums[start]
+	l, r := start, stop
+	for l < r{
+		for l < r && nums[r] >= pivot {
+			r--
+		}
+		nums[l] = nums[r]
+		for l < r && nums[l] < pivot {
+			l++
+		}
+		nums[r] = nums[l]
+	}
+	nums[l] = pivot
+	return l
+}
+
+func TopKSplit(nums []int, k, start, stop int){
+	if start < stop{
+		index := Parition(nums, start, stop)
+
+		if index == k {
+			return
+		} else if index < k {
+			TopKSplit(nums, k,index+1, stop )
+		} else if index > k {
+			TopKSplit(nums, k , start , index - 1)
+		}
+	}
+}
+
